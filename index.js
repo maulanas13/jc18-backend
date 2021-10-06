4a22720fbeb35636328fa8d07e60395bb0afcd49
maulanas <maulana@troopergears.com>
2021-10-06 17:55:11 +0700

    Belajar CRUD & integrasi BE to FE

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 5100;
 const { renderHtml } = require("./helpers");
 
@@ -19,16 +20,26 @@ let users = [
 let products = [
   {
     id: 1,
-    name: "pensil",
-    keterangan: "untuk nulis",
+    name: "Pensil",
+    price: 5000,
   },
   {
     id: 2,
-    name: "rautan",
-    keterangan: "bikin pensil tajem",
+    name: "Rautan",
+    price: 13000,
+  },
+  {
+    id: 3,
+    name: "Papan Tulis",
+    price: 110000,
+  },
+  {
+    id: 4,
+    name: "Projector",
+    price: 1230000,
   },
 ];
-let id = 2;
+let id = 4;
 const loggingFunc = (req, res, next) => { // Function utk record console log, guna utk troubleshoot backend ketika request user ada error
     console.log(req.method, req.url, new Date().toString());
     req.bambang = "namaku"; // nambah property bambang di object req
@@ -37,6 +48,7 @@ const loggingFunc = (req, res, next) => { // Function utk record console log, gu
 
 // Pasang middleware
 app.use(express.json()); // Middleware global start
+app.use(cors()); // Klo kyk gini, semua backend bs keakses, kita bisa ksh whitelist sendiri
 app.use(loggingFunc);
 // ini midlleware untuk nampung data body untuk method post,put,patch
 
@@ -55,9 +67,32 @@ app.get("/", async (req, res) => {
 });
 
 app.get("/products", (req, res) => {
-    console.log(req.bambang, "dari function sebelumnya");
-    return res.status(200).send(products);
+    console.log("Line 68, req.query products", req.query);
+    const {namaProd, hargaMin, hargaMax} = req.query;
+    console.log(Boolean(namaProd), "hasil");
+    let newFilterProd = products;
+    if (!namaProd && !hargaMin && !hargaMax) {
+        return res.status(200).send(products);
+    }
+    if (namaProd) {
+        newFilterProd = newFilterProd.filter((val) => 
+            val.name.toLowerCase().includes(namaProd.toLowerCase())
+        );
+    }
+    if (hargaMin) {
+        newFilterProd = newFilterProd.filter((val) => val.price >= hargaMin);
+    }
+    if (hargaMax) {
+        newFilterProd = newFilterProd.filter((val) => val.price >= hargaMax);
+    }
+    return res.status(200).send(newFilterProd);
 });
+
+app.get("/products/:id", (req,res) => {
+    const {id} = req.params;
+    let indexProd = products.findIndex((val) => val.id == id);
+    return res.status(200).send(products[indexProd]);
+})
 
 app.get("/users", (req, res) => {
     console.log("query user", req.query);
@@ -70,6 +105,27 @@ app.post("/products", (req, res) => {
     data.id = ++id;
     products.push(data); // Push data to database
     return res.status(200).send(products);
+});
+
+app.patch("/products/:id", (req, res) => {
+    const {id} = req.params;
+    let indexEdit = products.findIndex((val) => val.id == id);
+
+    if (indexEdit >= 0) {
+        const {name, price} = req.body;
+        if (name) {
+            products[indexEdit].name = name;
+        }
+        if (price) {
+            products[indexEdit].price = price;
+        }
+        return res.status(200).send()
+    } else {
+        let obj = {
+            message: "tidak ada id",
+        };
+        return res.status(400).send(obj);
+    }
 });
 
 app.delete("/products/:id", (req, res) => { // Bisa terima params kyk react router dom
